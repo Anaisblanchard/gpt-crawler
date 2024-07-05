243b4fc989536634fe7a06929e93d8ac59311fc4
Anaisblanchard <anais.blanchard.pro@gmail.com>
2024-07-05 18:14:11 +0200

    Update config.ts LPV
    Analyse des pages blog LPV

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  url: "https://www.lepetitvapoteur.com/fr/blog",
+  match: "https://www.lepetitvapoteur.com/fr/blog/**",
+  maxPagesToCrawl: 500,
   outputFileName: "output.json",
   maxTokens: 2000000,
 };
